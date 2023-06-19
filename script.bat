23782871ba4681b8d3f65fcc2a8f477df03eae19
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-06-19 14:22:18 -0600

    feat: run script

--- script.bat
+++ script.bat
@@ -1,4 +1,4 @@
 @echo off
 
-start cmd /c "cd client && npm run dev"
-start cmd /c "cd server && npm run dev"
+start cmd /c "cd client && npm i && npm run dev"
+start cmd /c "cd server && npm i && npm run dev"
