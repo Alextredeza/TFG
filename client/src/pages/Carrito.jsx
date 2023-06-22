1694ebf25e94b80aca25b90218197a8d0facb553
Kamerr Ezz <kamerr_oficial@hotmail.com>
2023-06-22 15:09:43 -0600

    feat: 6

--- client/src/pages/Carrito.jsx
+++ client/src/pages/Carrito.jsx
@@ -1,59 +1,92 @@
-import React from 'react'
-import { useApp } from '../hooks/useApp'
-import { BiX } from 'react-icons/bi'
-import Layout from '../components/Layouts/Layout'
-import { useLocalStorage } from '../hooks/useLocalStorage'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useApp } from "../hooks/useApp";
+import { BiX } from "react-icons/bi";
+import Layout from "../components/Layouts/Layout";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+import { useLocation } from "react-router-dom";
 
 function Carrito() {
+  const location = useLocation();
 
-  const location = useLocation()
+  const { dataLocalStorage, removeCardStore } = useApp();
+  const { saveData: saveDATA } = useLocalStorage("shop", false);
 
-  const { dataLocalStorage, removeCardStore } = useApp()
-  const {
-    saveData: saveDATA,
-  } = useLocalStorage('shop', false)
-
-  if (dataLocalStorage.length === 0) return <Layout><p className='text-white text-xl p-10'>No tienes nada en tu carrito de compras</p>
-</Layout>
+  if (dataLocalStorage.length === 0)
+    return (
+      <Layout>
+        <p className="text-white text-xl p-10">
+          No tienes nada en tu carrito de compras
+        </p>
+      </Layout>
+    );
   return (
     <Layout>
-    <div className='container m-auto p-3'>
-      <div className='p-3 gap-2 grid lg:grid-cols-2 grid-cols-1'>
-        {dataLocalStorage.map((item, index) => {
-          return <div key={index} className='p-2 bg-slate-300 rounded-md w-full flex gap-2  relative'>
-            <img src={item.img} alt="imagen de coche" className='rounded-md w-[50%]' />
-            <div className='flex gap-2 flex-col'>
-              <p className='font-bold text-xl' >{item.model}</p>
-              <p className='text-sm' >{item.brand}</p>
-              <p className='text-red-500 text-2xl font-bold' >€ {item.price}</p>
-              <button
-                onClick={() => {
-                  removeCardStore(item.uuid)
-                }}
-                className='absolute top-0 right-0' >
-                <BiX className='font-bold text-2xl text-red-500' />
-              </button>
-            </div>
+      <div className="container m-auto p-3">
+        <div className="flex flex-col gap-4 lg:flex-row">
+          <div className="bg-[#242335] p-2 rounded-md text-white w-full lg:w-80 h-fit">
+            <p className="text-2xl font-bold">Resumen de compra</p>
+            <ul className="mt-2">
+              {dataLocalStorage.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className="flex justify-between text-sm text-white/50"
+                  >
+                    <p>
+                      {item.model} {item.brand}
+                    </p>
+                    <p>€ {item.price}</p>
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="border-t mt-4">
+              Total:{" "}
+              {dataLocalStorage.reduce((acc, item) => acc + item.price, 0)}
+            </p>
           </div>
-        })}
+          <div className="bg-[#242335] p-2 rounded-md text-white w-full lg:w-[calc(100vw_-_20rem)] h-fit">
+            {dataLocalStorage.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="p-2 rounded-md w-full flex gap-2 relative h-14"
+                >
+                    <div className="aspect-video">
+                      <img
+                        src={item.img}
+                        alt="imagen de coche"
+                        className="rounded-md w-full"
+                      />
+                    </div>
+                    <div className="flex flex-col justify-center">
+                      <div>
+                        <p className="font-bold text-xl">
+                          {item.model} {item.brand}{" "}
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-red-500 text-sm font-bold">
+                          € {item.price}
+                        </p>
+                      </div>
+                    </div>
+                  <button
+                    onClick={() => {
+                      removeCardStore(item.uuid);
+                    }}
+                    className="absolute top-0 right-0"
+                  >
+                    <BiX className="font-bold text-2xl text-red-500" />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      <div className='bg-paletter-bluesecond p-2 rounded-md text-white flex justify-between'>
-        <p className='text-2xl ' ><span className='font-bold'>Total</span>: € {dataLocalStorage.reduce((acc, item) => acc + item.price, 0)}</p>
-        
-        <button
-          className='bg-green-500 text-white rounded-md p-2 hover:bg-green-600'
-          onClick={() => {
-            saveDATA(dataLocalStorage)
-            window.location.href = '/shop' 
-          }}
-        >
-          Comprar
-        </button>
-      </div>
-    </div>
     </Layout>
-  )
+  );
 }
 
-export default Carrito
+export default Carrito;
